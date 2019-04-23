06c35f5d3dcf4079b5eaeec65911bb08e6439b4d
Jie Bing <bingjie3216@gmail.com>
2019-04-22 21:21:33 -0700

    Initial Commit

---
+++ USCISStatusPredictor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  USCISStatusPredictor
+//
+//  Created by ginny lee on 4/22/19.
+//  Copyright © 2019 bing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
